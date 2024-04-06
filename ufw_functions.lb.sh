874d2b830f6c84f5cd67361771a8efb2ffce63fd
Junoo9 <charbelrahme16@gmail.com>
2024-04-06 13:52:30 +0300

    Added some error messages to inform users and some fixes

--- ufw_functions.lb.sh
+++ ufw_functions.lb.sh
@@ -1,31 +1,30 @@
-#!/bin/bash 
+#!/bin/bash
 #
 # UFW Functions: Library of bash functions that wraps UFW and IP-Tables functionalities
 #
 # Anthony Debbas, Charbel Rahme, Paul A. Estephan, Peter G. Chalhoub
 # CVS:$Header$
 
-UFW_Exist (){
-  printf "\033[32mChecking if UFW exists...\033[0m\n"
-  # Scrap the default error message
-  sudo ufw &> /dev/null 
-  # if not exist error: 127  install ufw 
-  if [[ $? -eq 127 ]]; then 
-    # For Debian based systems 
-    sudo apt install ufw -y &> /dev/null 
-    if [[ $? -eq 0 ]]; then 
-      printf "UFW is now installed.\n You may proceed."
+UFW_Exist () {
+  echo -e "\033[32mChecking if UFW exists...\033[0m"
+  if ! command -v ufw &> /dev/null; then
+    # UFW is not installed
+    # For Debian based systems
+    sudo apt install ufw -y &> /dev/null
+    if [[ $? -eq 0 ]]; then
+      echo "UFW is now installed. You may proceed."  # Inform the dummy user that UFW is installed
+    else
+      echo "Failed to install UFW."  # Inform the dummy user that UFW installation failed
     fi
-  elif [[ $? -eq 1 ]]; then 
-    printf "UFW is already installed. To stop the process of checking if UFW is installed comment the UFW_Exist function in ufw_menu.sh"
-  fi 
-
+  else
+    echo "UFW is already installed. To stop the process of checking if UFW is installed, comment out the UFW_Exist function in ufw_menu.sh."  # Inform a stupid user that UFW is already installed
+  fi
 }
 
 UFW_Start () {
-  sudo ufw enable > ufw_start.txt 
+  sudo ufw enable > ufw_start.txt
   dialog --title "UFW Start" --textbox ufw_start.txt 22 70
-  rm ufw_start.txt 
+  rm ufw_start.txt
 }
 
 UFW_Status_Check () {
@@ -35,7 +34,7 @@ UFW_Status_Check () {
 }
 
 UFW_Stop () {
-  sudo ufw disable > ufw_stop.txt 
+  sudo ufw disable > ufw_stop.txt
   dialog --title "UFW Stop" --textbox ufw_stop.txt 22 70
   rm ufw_stop.txt
 }
@@ -43,5 +42,4 @@ UFW_Stop () {
 # UFW_Manage_Rule () {
 #
 # }
-
 
